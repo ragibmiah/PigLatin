746e9628700a6fbc5e4d9cb01d8b8a63984b25a0
Ragib Miah <ragib@umich.edu>
2018-04-30 08:47:48 -0400

    Second commit for PigLatin

--- CapstonePigLatin/Program.cs
+++ CapstonePigLatin/Program.cs
@@ -14,13 +14,20 @@ namespace CapstonePigLatin
         }
     static void Translate()
         {
-
+            //Using the regular expression to omit numbers but it cant't get numbers within words.
             Regex specialCharacter = new Regex("^[0-9]*$");
+            //This is to check my vowels.
             string vowels = "aeiouAEIOU";
 
             Console.Write("\nEnter a word to be translated: ");
             string input = Console.ReadLine().ToLower();
+            //Using to lower the letters for less error.
 
+            string[] stringArray = input.Split(' ');
+            //My attempt to break into sentences. did not work like i planned.
+
+
+            //For every time a person puts nothing in, they get asked to try again.
             if (input == "")
             {
                 Translate();
@@ -28,10 +35,12 @@ namespace CapstonePigLatin
             else if (specialCharacter.IsMatch(input))
             {
                 Console.WriteLine(input);
+                //This finds numbers but not within words.
             }
             else if (input.Substring(0, 1) == "a" || input.Substring(0, 1) == "e" || input.Substring(0, 1) == "i" || input.Substring(0, 1) == "o" || input.Substring(0, 1) == "u" || input.Substring(0, 1) == "A" || input.Substring(0, 1) == "E" || input.Substring(0, 1) == "I" || input.Substring(0, 1) == "O" || input.Substring(0, 1) == "U")
             {
                 Console.WriteLine(input + "way");
+                //Used the long way to check for vowels in the beginning of words.
             }
             else
             {
@@ -48,7 +57,7 @@ namespace CapstonePigLatin
                             Console.WriteLine(pigLatin + firstLetter + "ay");
                             j = vowels.Length;
                             i = input.Length;
-                           
+                           //This is used to check for vowels after consonants.
                         }
 
                     }
